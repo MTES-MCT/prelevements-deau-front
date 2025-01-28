46f34922ff24dae4bc916c4152e9d2cff161bfd5
Guillaume <guillaume@livingdata.co>
2025-01-28 16:28:31 +0100

    Adds colors in an Object to avoid duplication

---
+++ src/components/map/legend-colors.js
@@ -0,0 +1,16 @@
+export const legendColors = {
+  usages: [
+    {text: 'Eau potable', color: 'deepskyblue'},
+    {text: 'Agriculture', color: 'lawngreen'},
+    {text: 'Camion citerne', color: 'grey'},
+    {text: 'Eau embouteillée', color: 'skyblue'},
+    {text: 'Hydroélectricité', color: 'steelblue'},
+    {text: 'Industrie', color: 'slategrey'},
+    {text: 'Non renseigné', color: 'lightgrey'},
+    {text: 'Thermalisme', color: 'lightseagreen'}
+  ],
+  typesMilieu: [
+    {text: 'Eau de surface', color: 'deepskyblue'},
+    {text: 'Eau souterraine', color: 'lightseagreen'}
+  ]
+}
