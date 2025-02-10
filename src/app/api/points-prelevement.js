0a8c5f164eebae15c6c669a56268f38d3f3d623d
Guillaume <guillaume@livingdata.co>
2025-02-10 16:01:33 +0100

    Adds new functions

--- src/app/api/points-prelevement.js
+++ src/app/api/points-prelevement.js
@@ -5,3 +5,29 @@ export async function getPointsPrelevement() {
   const dossiers = await response.json()
   return dossiers
 }
+
+export async function getPointPrelevement(id) {
+  const response = await fetch(`${API_URL}/api/points-prelevement/${id}`)
+  const dossier = await response.json()
+  return dossier
+}
+
+export async function getBss(id) {
+  try {
+    const response = await fetch(`${API_URL}/api/bss/${id}`)
+    const bss = await response.json()
+    return bss
+  } catch {
+    return null
+  }
+}
+
+export async function getBnpe(id) {
+  try {
+    const response = await fetch(`${API_URL}/api/bnpe/${id}`)
+    const ouvrage = await response.json()
+    return ouvrage
+  } catch {
+    return null
+  }
+}
