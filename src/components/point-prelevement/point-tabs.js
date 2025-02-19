2c29cbd5c67db388311d2fbdfa54a1e2b0529eb9
Guillaume <guillaume@livingdata.co>
2025-02-19 10:44:25 +0100

    Removes mediaQueries in tabs

--- src/components/point-prelevement/point-tabs.js
+++ src/components/point-prelevement/point-tabs.js
@@ -1,24 +1,18 @@
 'use client'
 
-import {Tab, Tabs, useMediaQuery} from '@mui/material'
-import {useTheme} from '@mui/material/styles'
+import {Tab, Tabs} from '@mui/material'
 
-const PointTabs = ({selectedTab, handleSelectedTab}) => {
-  const theme = useTheme()
-  const isMobile = useMediaQuery(theme.breakpoints.down('sm'))
-
-  return (
-    <Tabs
-      value={selectedTab}
-      variant={isMobile ? 'scrollable' : 'standard'}
-      scrollButtons={isMobile ? 'auto' : 'off'}
-      onChange={handleSelectedTab}
-    >
-      <Tab value='identification' label='Identification' />
-      <Tab value='localisation' label='Localisation' />
-      <Tab value='exploitations' label='Exploitations' />
-    </Tabs>
-  )
-}
+const PointTabs = ({selectedTab, handleSelectedTab}) => (
+  <Tabs
+    value={selectedTab}
+    variant='scrollable'
+    scrollButtons='auto'
+    onChange={handleSelectedTab}
+  >
+    <Tab value='identification' label='Identification' />
+    <Tab value='localisation' label='Localisation' />
+    <Tab value='exploitations' label='Exploitations' />
+  </Tabs>
+)
 
 export default PointTabs
