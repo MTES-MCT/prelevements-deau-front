d671f8a3592851737636f164d0fcd4066ebe8a0d
Guillaume <guillaume@livingdata.co>
2025-02-19 17:18:46 +0100

    Make legend closable

--- src/components/map/legend.js
+++ src/components/map/legend.js
@@ -1,3 +1,7 @@
+import {useState} from 'react'
+
+import AddCircleOutline from '@mui/icons-material/AddCircleOutline'
+import Cancel from '@mui/icons-material/Cancel'
 import {Box, useTheme} from '@mui/material'
 
 import {legendColors} from './legend-colors.js'
@@ -20,6 +24,7 @@ const Bubble = ({color, text}) => (
 const Legend = () => {
   const {usages} = legendColors
   const theme = useTheme()
+  const [isOpen, setIsOpen] = useState(true)
 
   return (
     <Box
@@ -31,15 +36,31 @@ const Legend = () => {
         backgroundColor: theme.palette.background.default
       }}
     >
-      <Box>
-        {usages.map(usage => (
-          <Box key={usage.text}>
-            <Bubble
-              color={usage.color}
-              text={usage.text}
-            />
-          </Box>
-        ))}
+      {isOpen && (
+        <Box sx={{p: 1, borderBottom: '1px solid grey'}}>
+          {usages.map(usage => (
+            <Box key={usage.text}>
+              <Bubble
+                color={usage.color}
+                text={usage.text}
+              />
+            </Box>
+          ))}
+        </Box>
+      )}
+      <Box
+        sx={{
+          cursor: 'pointer',
+          pt: isOpen ? 1 : 0
+        }}
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        {isOpen
+          ? <Cancel className='align-bottom' />
+          : <AddCircleOutline className='align-bottom' />}
+        <span className='pl-2'>
+          {isOpen ? 'Fermer' : 'Afficher la légende'}
+        </span>
       </Box>
     </Box>
   )
