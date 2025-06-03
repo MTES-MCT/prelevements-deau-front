d11a4c730f2851cc96060996e026d56d79da6686
Guillaume <guillaume@livingdata.co>
2025-06-03 11:40:56 +0200

    Fix linting

--- src/app/prelevements/[id]/page.js
+++ src/app/prelevements/[id]/page.js
@@ -1,5 +1,6 @@
-import {getExploitationsByPointId, getPointPrelevement} from '@/app/api/points-prelevement.js'
 import Loading from './loading.js'
+
+import {getExploitationsByPointId, getPointPrelevement} from '@/app/api/points-prelevement.js'
 import PointExploitations from '@/components/prelevements/point-exploitations.js'
 import PointIdentification from '@/components/prelevements/point-identification.js'
 import PointLocalisation from '@/components/prelevements/point-localisation.js'
