f6ef83e386255d8fbff95bef167d450617fdc62f
Théophile Merlière <theophile.merliere@gmail.com>
2025-06-09 15:46:18 +0200

    Rename DossierModal into DossierDetails

--- src/app/dossiers/[dossierId]/page.js
+++ src/app/dossiers/[dossierId]/page.js
@@ -2,23 +2,27 @@ import {Button} from '@codegouvfr/react-dsfr/Button'
 import {Typography} from '@mui/material'
 
 import {getDossier} from '@/app/api/dossiers.js'
-import DossierModal from '@/components/declarations/dossier-modal.js'
+import {getPointPrelevement} from '@/app/api/points-prelevement.js'
+import DossierDetails from '@/components/declarations/dossier-details.js'
+import BackButton from '@/components/ui/back-button.js'
 import {getDossierDSURL, getDossiersURL} from '@/lib/urls.js'
 
 const DossierPage = async ({params}) => {
   const {dossierId} = await params
+
   const dossier = await getDossier(dossierId)
+
+  let pointPrelevement
+  if (dossier.pointPrelevement) {
+    pointPrelevement = await getPointPrelevement(dossier.pointPrelevement)
+  }
 
   return (
     <div className='fr-container mt-4'>
-      <Button
-        iconId='fr-icon-arrow-go-back-fill'
-        linkProps={{
-          href: getDossiersURL()
-        }}
-      >
-        Retour à la liste des dossiers
-      </Button>
+      <BackButton
+        label='Retour à la liste des dossiers'
+        href={getDossiersURL()}
+      />
 
       <div className='flex items-end justify-between flex-wrap'>
         <Typography className='text-center pt-10' variant='h3'>Dossier n°{dossier.numero}</Typography>
@@ -33,7 +37,10 @@ const DossierPage = async ({params}) => {
       </div>
 
       <div className='my-4'>
-        <DossierModal selectedDossier={dossier} />
+        <DossierDetails
+          dossier={dossier}
+          pointPrelevement={pointPrelevement}
+        />
       </div>
     </div>
   )
