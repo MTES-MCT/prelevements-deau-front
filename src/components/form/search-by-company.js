2ec789925120e03b7bd203ffb702e755ce22039c
Guillaume <guillaume@livingdata.co>
2025-06-30 12:10:58 +0200

    Update results style in SearchByCompany

--- src/components/form/search-by-company.js
+++ src/components/form/search-by-company.js
@@ -24,17 +24,19 @@ const SearchByCompany = ({setPreleveur}) => {
   }, 300)
 
   const handleCompanyInfos = value => {
-    setPreleveur(prev => ({
-      ...prev,
-      raison_sociale: value.nom_raison_sociale,
-      code_siren: value.siren,
-      nom: value?.dirigeants?.[0]?.nom || '',
-      prenom: value?.dirigeants?.[0]?.prenoms || '',
-      adresse_1: `${value.siege.numero_voie || ''} ${value.siege.type_voie || ''} ${value.siege.libelle_voie || ''}`,
-      adresse_2: value.siege.complement_adresse,
-      code_postal: value.siege.code_postal,
-      commune: value.siege.libelle_commune
-    }))
+    if (value) {
+      setPreleveur(prev => ({
+        ...prev,
+        raison_sociale: value.nom_raison_sociale,
+        code_siren: value.siren,
+        nom: value?.dirigeants?.[0]?.nom || '',
+        prenom: value?.dirigeants?.[0]?.prenoms || '',
+        adresse_1: `${value.siege.numero_voie || ''} ${value.siege.type_voie || ''} ${value.siege.libelle_voie || ''}`,
+        adresse_2: value.siege.complement_adresse,
+        code_postal: value.siege.code_postal,
+        commune: value.siege.libelle_commune
+      }))
+    }
   }
 
   return (
@@ -44,17 +46,27 @@ const SearchByCompany = ({setPreleveur}) => {
         renderInput={({className, id, placeholder, type}) => (
           <SearchAutocomplete
             noOptionsText='Pas de résultat'
-            options={companies.map(company => ({
-              company,
-              label: `${company.nom_complet} (${company.siren})`
-            }))}
+            options={companies}
+            getOptionLabel={company => `${company.nom_complet} (${company.siren})`}
             getOptionKey={c => c.siren}
+            renderOption={(props, company) => {
+              const {key, ...otherProps} = props
+              return (
+                <div key={company.siren} {...otherProps} className='p-3 even:bg-[#f5f5fe]'>
+                  <div className='flex flex-cols justify-between'>
+                    <p>{company.nom_complet}</p>
+                    <p>{company.siege.code_postal} - {company.siege.libelle_commune}</p>
+                  </div>
+                  <small className='text-gray-500'>SIREN {company.siren}</small>
+                </div>
+              )
+            }}
             className={className}
             id={id}
             placeholder={placeholder}
             type={type}
             loading={isLoading}
-            onChange={(e, value) => handleCompanyInfos(value?.company)}
+            onChange={(e, value) => handleCompanyInfos(value)}
           />
         )}
         onChange={e => fetchCompanies(e.target.value)}
