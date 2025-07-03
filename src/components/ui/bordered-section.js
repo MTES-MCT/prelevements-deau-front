e56fec0117509ef0945e4006a8fe55d5e3fe5961
Maxine <maxine.lepennec@gmail.com>
2025-07-03 14:57:36 +0200

    Create bordered section component

---
+++ src/components/ui/bordered-section.js
@@ -0,0 +1,33 @@
+'use client'
+
+import {fr} from '@codegouvfr/react-dsfr'
+import {Button} from '@codegouvfr/react-dsfr/Button'
+import {Typography, Box} from '@mui/material'
+import Link from 'next/link'
+
+const BorderedSection = ({title, linkPath, handleAction, icon, buttonLabel, children}) => (
+  <Box sx={{display: 'flex', flexDirection: 'column', gap: 1}}>
+    <Box sx={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
+      <Typography variant='h6'>{title}</Typography>
+      {handleAction ? (
+        <Button
+          priority='secondary'
+          iconId={`fr-icon-${icon}`}
+          onClick={handleAction}
+        >
+          {buttonLabel}
+        </Button>
+      ) : (
+        <Link href={linkPath} className='fr-btn fr-btn--secondary'>
+          <span className={`fr-icon-${icon}`} /> {buttonLabel}
+        </Link>
+      )}
+    </Box>
+
+    <Box sx={{border: `1px solid ${fr.colors.decisions.border.default.grey.default}`, padding: 2}}>
+      {children}
+    </Box>
+  </Box>
+)
+
+export default BorderedSection
