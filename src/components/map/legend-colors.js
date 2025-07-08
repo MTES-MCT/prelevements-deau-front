6458e8d15444f8f77aff1933a609c1260f11b01b
Maxine <maxine.lepennec@gmail.com>
2025-07-08 09:32:29 +0200

    Fix Camion citerne color

--- src/components/map/legend-colors.js
+++ src/components/map/legend-colors.js
@@ -5,7 +5,7 @@ export const legendColors = {
   usages: [
     {text: 'Eau potable', color: 'var(--background-flat-blue-cumulus)', textColor: lightTextColor},
     {text: 'Agriculture', color: 'var(--background-flat-green-archipel)', textColor: lightTextColor},
-    {text: 'Camion citerne', color: 'var(--artwork-major-green-archipel-hover)', textColor: lightTextColor},
+    {text: 'Camion citerne', color: 'var(--artwork-minor-purple-glycine)', textColor: lightTextColor},
     {text: 'Eau embouteillée', color: 'var(--artwork-motif-purple-glycine)', textColor: defaultTextColor},
     {text: 'Hydroélectricité', color: 'var(--background-contrast-yellow-moutarde-active)', textColor: defaultTextColor},
     {text: 'Industrie', color: 'var(--artwork-major-red-marianne-active)', textColor: lightTextColor},
