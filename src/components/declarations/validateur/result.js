7cc9988acdb0f0e994016e5e7305a2bca5cea245
Théophile Merlière <theophile.merliere@gmail.com>
2025-07-15 15:23:23 +0200

    Display file data

--- src/components/declarations/validateur/result.js
+++ src/components/declarations/validateur/result.js
@@ -4,10 +4,12 @@ import {
 } from '@mui/material'
 import {Box} from '@mui/system'
 
+import PrelevementsAccordion from '@/components/declarations/dossier/prelevements/prelevements-accordion.js'
+import Spreadsheet from '@/components/declarations/dossier/prelevements/spreadsheet.js'
 import FileValidationErrors from '@/components/declarations/file-validation-errors.js'
 import {formatBytes} from '@/utils/size.js'
 
-const ValidateurResult = ({file, errors = []}) => {
+const ValidateurResult = ({file, fileType, pointPrelevement, data, errors = []}) => {
   const noError = errors.length === 0
   return (
     <div className='flex flex-col gap-4'>
@@ -20,7 +22,7 @@ const ValidateurResult = ({file, errors = []}) => {
         />
       )}
 
-      {file && errors.length > 0 && (
+      {file && errors.length > 0 && !data && (
         <Box className='flex flex-col gap-4'>
           <Card variant='outlined'>
             <CardContent>
@@ -34,6 +36,26 @@ const ValidateurResult = ({file, errors = []}) => {
             <FileValidationErrors errors={errors} />
           </Card>
         </Box>
+      )}
+
+      {data && (
+        <PrelevementsAccordion
+          isOpen
+          idPoint={data.pointPrelevement}
+          pointPrelevement={pointPrelevement}
+          volumePreleveTotal={data.volumePreleveTotal}
+          status={errors?.length > 0 || !data ? 'error' : 'success'}
+        >
+          {fileType === 'Données standardisées' ? (
+            <Spreadsheet
+              data={data}
+              errors={errors}
+            />
+          ) : (
+            <Alert severity='info' description=' Ce type de dossier n’est pas encore pris en charge.' />
+          )}
+
+        </PrelevementsAccordion>
       )}
     </div>
   )
