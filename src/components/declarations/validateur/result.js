db1e3a722ac5fa713dbc971af6e365e285b75e02
Théophile Merlière <theophile.merliere@gmail.com>
2025-07-15 16:10:55 +0200

    Update result display

--- src/components/declarations/validateur/result.js
+++ src/components/declarations/validateur/result.js
@@ -12,52 +12,49 @@ import {formatBytes} from '@/utils/size.js'
 const ValidateurResult = ({file, fileType, pointPrelevement, data, errors = []}) => {
   const noError = errors.length === 0
   return (
-    <div className='flex flex-col gap-4'>
-      {errors && (
-        <Alert
-          closable={false}
-          title={noError ? 'Le fichier est valide' : 'Le fichier est invalide'}
-          description={noError ? 'Aucune erreur détectée' : `Le fichier contient ${errors.length} erreur${errors.length > 1 ? 's' : ''}`}
-          severity={noError ? 'success' : 'error'}
-        />
-      )}
+    <Box className='flex flex-col gap-4'>
+      <Alert
+        closable={false}
+        title={noError ? 'Le fichier est valide' : 'Le fichier est invalide'}
+        description={noError ? 'Aucune erreur détectée' : `Le fichier contient ${errors.length} erreur${errors.length > 1 ? 's' : ''}`}
+        severity={noError ? 'success' : 'error'}
+      />
 
-      {file && errors.length > 0 && !data && (
-        <Box className='flex flex-col gap-4'>
-          <Card variant='outlined'>
-            <CardContent>
-              <Typography gutterBottom variant='h5' component='div'>
-                {file.name}
-              </Typography>
-              <Typography variant='body2' sx={{color: 'text.secondary'}}>
-                {formatBytes(file.size)}
-              </Typography>
-            </CardContent>
-            <FileValidationErrors errors={errors} />
-          </Card>
-        </Box>
-      )}
+      <Card variant='outlined'>
+        <CardContent>
+          <Typography gutterBottom variant='h5' component='div'>
+            {file.name}
+          </Typography>
+          <Typography variant='body2' sx={{color: 'text.secondary'}}>
+            {formatBytes(file.size)}
+          </Typography>
+        </CardContent>
 
-      {data && (
-        <PrelevementsAccordion
-          isOpen
-          idPoint={data.pointPrelevement}
-          pointPrelevement={pointPrelevement}
-          volumePreleveTotal={data.volumePreleveTotal}
-          status={errors?.length > 0 || !data ? 'error' : 'success'}
-        >
-          {fileType === 'Données standardisées' ? (
-            <Spreadsheet
-              data={data}
-              errors={errors}
-            />
-          ) : (
-            <Alert severity='info' description=' Ce type de dossier n’est pas encore pris en charge.' />
-          )}
+        {errors.length > 0 && !data && (
+          <FileValidationErrors errors={errors} />
+        )}
 
-        </PrelevementsAccordion>
-      )}
-    </div>
+        {data && (
+          <PrelevementsAccordion
+            isOpen
+            idPoint={data.pointPrelevement}
+            pointPrelevement={pointPrelevement}
+            volumePreleveTotal={data.volumePreleveTotal}
+            status={errors?.length > 0 || !data ? 'error' : 'success'}
+          >
+            {fileType === 'Données standardisées' ? (
+              <Spreadsheet
+                data={data}
+                errors={errors}
+              />
+            ) : (
+              <Alert severity='info' description=' Ce type de dossier n’est pas encore pris en charge.' />
+            )}
+
+          </PrelevementsAccordion>
+        )}
+      </Card>
+    </Box>
   )
 }
 
