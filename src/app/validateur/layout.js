05896d946adedccae63d1512bd805ce54e72faa9
Théophile Merlière <theophile.merliere@gmail.com>
2025-07-16 12:09:43 +0200

    Move title and description to layout

--- src/app/validateur/layout.js
+++ src/app/validateur/layout.js
@@ -1,4 +1,5 @@
 import {Notice} from '@codegouvfr/react-dsfr/Notice'
+import {Typography} from '@mui/material'
 
 import {StartDsfrOnHydration} from '@/dsfr-bootstrap/index.js'
 
@@ -11,7 +12,17 @@ const Validateur = async ({children}) => (
       title='Cet outil est en cours de développement.'
       description='Merci de nous signaler toute erreur non détectée ou injustifiée.'
     />
-    {children}
+
+    <div className='fr-container flex flex-col my-4 gap-10'>
+      <Typography variant='h3'>
+        Validateur de fichier de déclaration de prélèvements
+      </Typography>
+      <p>
+        Cet outil vous permet de valider la conformité de vos fichiers de déclaration de prélèvements avant de les soumettre sur Démarches Simplifiées.
+      </p>
+
+      {children}
+    </div>
   </>
 )
 
