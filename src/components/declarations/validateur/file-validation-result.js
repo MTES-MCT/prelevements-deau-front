5dc8635475be67e762aaaf9f6302b78f109a83e0
Théophile Merlière <theophile.merliere@gmail.com>
2025-07-28 16:12:35 +0200

    Linter

--- src/components/declarations/validateur/file-validation-result.js
+++ src/components/declarations/validateur/file-validation-result.js
@@ -1,4 +1,6 @@
-import {useCallback, useEffect, useRef, useState} from 'react'
+import {
+  useCallback, useEffect, useRef, useState
+} from 'react'
 
 import Button from '@codegouvfr/react-dsfr/Button'
 import {Card} from '@mui/material'
