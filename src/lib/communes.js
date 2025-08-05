d0ec022fdd64d4a69df112494420dd2bfdade033
Guillaume <guillaume@livingdata.co>
2025-08-05 17:40:36 +0200

    Fix communes object

--- src/lib/communes.js
+++ src/lib/communes.js
@@ -2,5 +2,8 @@ export async function getCommuneFromCoords({lat, lon}) {
   const response = await fetch(`https://data.geopf.fr/geocodage/reverse?index=poi&category=commune&lon=${lon}&lat=${lat}`)
   const communes = await response.json()
 
-  return communes?.features[0]?.properties?.name[0]
+  return {
+    nom: communes?.features[0]?.properties?.name[0],
+    code: communes?.features[0]?.properties?.citycode[0]
+  }
 }
