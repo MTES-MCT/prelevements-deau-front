6dd457b4c0425e7650b229712893f6ddf7142f17
Jérôme Desboeufs <jerome@livingdata.co>
2025-08-27 12:04:51 +0200

    Complete storybook configuration

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,14 +2,19 @@ const config = {
   "stories": [
     "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
   ],
-  "addons": [],
+  "addons": [
+    "@storybook/addon-docs"
+  ],
   "framework": {
     "name": "@storybook/nextjs",
     "options": {}
   },
   "staticDirs": [
     "../public"
-  ]
+  ],
+  "docs": {
+    "autodocs": "tag"
+  }
 }
 
 export default config
