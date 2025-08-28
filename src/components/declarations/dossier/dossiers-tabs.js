0cdc2f9985fb4d568af95570c82dc44cc60d1c07
Guillaume <guillaume@livingdata.co>
2025-08-28 16:53:49 +0200

    Update DossiersTabs

--- src/components/declarations/dossier/dossiers-tabs.js
+++ src/components/declarations/dossier/dossiers-tabs.js
@@ -5,77 +5,46 @@ import {useState} from 'react'
 import {Tabs} from '@codegouvfr/react-dsfr/Tabs'
 import {Tag} from '@codegouvfr/react-dsfr/Tag'
 
+import DossiersFilters from '@/components/declarations/dossier/dossiers-filters.js'
 import DossiersList from '@/components/declarations/dossiers-list.js'
 
-const CounterTab = ({count}) => (
-  <Tag
-    dismissible // Pour avoir la couleur de fond bleue
-    small
-  >
-    {count}
-  </Tag>
-)
+const tabConfig = [
+  {tabId: 'en-instruction', label: 'En cours d’instruction', statsKey: 'en-instruction'},
+  {tabId: 'accepte', label: 'Acceptés', statsKey: 'accepte'},
+  {tabId: 'refuse', label: 'Refusés', statsKey: 'refuse'},
+  {tabId: 'en-construction', label: 'En construction', statsKey: 'en-construction'},
+  {tabId: 'sans-suite', label: 'Classés sans suite', statsKey: 'sans-suite'},
+  {tabId: 'archive', label: 'Archivés', statsKey: 'archive'}
+]
 
 const DossiersTabs = ({dossiersStats}) => {
   const [activeTab, setActiveTab] = useState('en-instruction')
+  const [filters, setFilters] = useState({})
 
   return (
-    <Tabs
-      selectedTabId={activeTab}
-      tabs={[
-        {
-          tabId: 'en-instruction',
+    <>
+      <DossiersFilters setFilters={setFilters} />
+      <Tabs
+        selectedTabId={activeTab}
+        tabs={tabConfig.map(tab => ({
+          tabId: tab.tabId,
           label: (
             <span>
-              <CounterTab count={dossiersStats['en-instruction'] || 0} /> En cours d’instruction
+              <Tag
+                dismissible
+                small
+              >
+                {dossiersStats[tab.statsKey] || 0}
+              </Tag>
+              <span className='fr-pl-1w'>{tab.label}</span>
             </span>
           )
-        },
-        {
-          tabId: 'accepte',
-          label: (
-            <span>
-              <CounterTab count={dossiersStats.accepte || 0} /> Acceptés
-            </span>
-          )
-        },
-        {
-          tabId: 'refuse',
-          label: (
-            <span>
-              <CounterTab count={dossiersStats.refuse || 0} /> Refusés
-            </span>
-          )
-        },
-        {
-          tabId: 'en-construction',
-          label: (
-            <span>
-              <CounterTab count={dossiersStats['en-construction'] || 0} /> En construction
-            </span>
-          )
-        },
-        {
-          tabId: 'sans-suite',
-          label: (
-            <span>
-              <CounterTab count={dossiersStats['sans-suite'] || 0} /> Classés sans suite
-            </span>
-          )
-        },
-        {
-          tabId: 'archive',
-          label: (
-            <span>
-              <CounterTab count={dossiersStats.archive || 0} /> Archivés
-            </span>
-          )
-        }
-      ]}
-      onTabChange={setActiveTab}
-    >
-      <DossiersList status={activeTab} />
-    </Tabs>
+        }))}
+        onTabChange={setActiveTab}
+      >
+        <DossiersList status={activeTab || 'en-instruction'} filters={filters} />
+      </Tabs>
+    </>
   )
 }
 
