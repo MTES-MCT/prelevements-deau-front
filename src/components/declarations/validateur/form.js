a71c6f6e8387f8211b9318e84244a7f0d54f4d46
Guillaume <guillaume@livingdata.co>
2025-09-02 14:38:56 +0200

    Fix typo

--- src/components/declarations/validateur/form.js
+++ src/components/declarations/validateur/form.js
@@ -43,7 +43,7 @@ const FileValidateurForm = ({isLoading, resetForm, handleSubmit}) => {
           value: fileType
         }}
       >
-        <option disabled hidden value=''>Selectionnez un type de fichier</option>
+        <option disabled hidden value=''>Sélectionnez un type de fichier</option>
         <option value='aep-zre'>Données standardisées (Tableau multi-paramètre)</option>
         <option value='camion-citerne'>Tableau de suivi camions citerne</option>
       </Select>
