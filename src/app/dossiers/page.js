3b18266225f851925b7b05fe227a8eef904e6027
Guillaume <guillaume@livingdata.co>
2025-09-02 11:04:35 +0200

    Removes dossiersStats

--- src/app/dossiers/page.js
+++ src/app/dossiers/page.js
@@ -1,31 +1,26 @@
 import {CallOut} from '@codegouvfr/react-dsfr/CallOut'
 
-import {getDossiersStats} from '@/app/api/dossiers.js'
 import DossiersTabs from '@/components/declarations/dossier/dossiers-tabs.js'
 import {StartDsfrOnHydration} from '@/dsfr-bootstrap/index.js'
 
 export const dynamic = 'force-dynamic'
 
-const Dossiers = async () => {
-  const dossiersStats = await getDossiersStats()
+const Dossiers = async () => (
+  <>
+    <StartDsfrOnHydration />
 
-  return (
-    <>
-      <StartDsfrOnHydration />
+    <div className='fr-container mt-4'>
+      <CallOut
+        iconId='ri-information-line'
+        title='Dossiers déposés'
+      >
+        Consultez, filtrez et triez les dossiers déposés par les préleveurs d’eau. Identifiez rapidement les erreurs éventuelles dans les données et accédez à leur détail pour un suivi précis.
+      </CallOut>
 
-      <div className='fr-container mt-4'>
-        <CallOut
-          iconId='ri-information-line'
-          title='Dossiers déposés'
-        >
-          Consultez, filtrez et triez les dossiers déposés par les préleveurs d’eau. Identifiez rapidement les erreurs éventuelles dans les données et accédez à leur détail pour un suivi précis.
-        </CallOut>
+      <DossiersTabs />
 
-        <DossiersTabs dossiersStats={dossiersStats} />
-
-      </div>
-    </>
-  )
-}
+    </div>
+  </>
+)
 
 export default Dossiers
