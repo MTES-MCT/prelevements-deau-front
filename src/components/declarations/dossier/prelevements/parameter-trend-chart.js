7cebd30876c7a63148566d31ef43ba198a8592c2
Maxine Le Pennec <66621960+Maxine-Le-Pennec@users.noreply.github.com>
2025-09-05 14:08:10 +0200

    Add PrelevementsHistory UI component (#205)
    * Add PrelevementsHistory UI component
    * Delete import
    * Delete useless 'use client'

--- src/components/declarations/dossier/prelevements/parameter-trend-chart.js
+++ src/components/declarations/dossier/prelevements/parameter-trend-chart.js
@@ -5,7 +5,7 @@ import {
 } from 'react'
 
 import {
-  Box, Divider, ToggleButton, ToggleButtonGroup, Slider
+  Box, ToggleButton, ToggleButtonGroup, Slider
 } from '@mui/material'
 import {LineChart} from '@mui/x-charts'
 import {
@@ -243,8 +243,6 @@ const ParameterTrendChart = ({data, connectNulls}) => {
 
   return (
     <Box className='flex flex-col gap-4 mt-4'>
-      <Divider textAlign='left'>Graphique de tendance des paramètres</Divider>
-
       {/* Sélecteur de granularité ----------------------------------------- */}
       <ToggleButtonGroup
         exclusive size='small'
