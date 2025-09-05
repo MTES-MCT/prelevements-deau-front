ad545c3dd4824f7b2d230e1a294ac704739489df
Maxine <maxine.lepennec@gmail.com>
2025-09-05 09:44:28 +0200

    Delete semicolon

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,7 +1,7 @@
-import "@codegouvfr/react-dsfr/main.css";
-import { startReactDsfr } from "@codegouvfr/react-dsfr/spa";
-import { MuiDsfrThemeProvider } from "@codegouvfr/react-dsfr/mui";
-import "../src/app/globals.css";
+import "@codegouvfr/react-dsfr/main.css"
+import { startReactDsfr } from "@codegouvfr/react-dsfr/spa"
+import { MuiDsfrThemeProvider } from "@codegouvfr/react-dsfr/mui"
+import "../src/app/globals.css"
 
 startReactDsfr({
     defaultColorScheme: "light",
