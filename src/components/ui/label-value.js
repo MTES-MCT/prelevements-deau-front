522aa65256b154c32e3372a645820618c7d7201f
Guillaume <guillaume@livingdata.co>
2025-09-15 11:49:33 +0200

    Fix LabelValue component usage

--- src/components/ui/label-value.js
+++ src/components/ui/label-value.js
@@ -1,12 +1,16 @@
 import {Box, Typography} from '@mui/material'
 
-const LabelValue = ({label, children}) => (
-  <Box className='flex flex-wrap items-center gap-2'>
-    <Typography variant='body1' color='text.secondary'>
-      <strong>{label} : </strong>
-    </Typography>
-    {children}
-  </Box>
-)
+const LabelValue = ({label, children}) => {
+  if (children) {
+    return (
+      <Box className='flex flex-wrap items-center gap-2'>
+        <Typography variant='body1' color='text.secondary'>
+          <strong>{label} : </strong>
+        </Typography>
+        {children}
+      </Box>
+    )
+  }
+}
 
 export default LabelValue
