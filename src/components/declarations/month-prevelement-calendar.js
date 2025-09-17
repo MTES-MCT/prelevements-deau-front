f57fa464ddb4e905aff5b92d654fbbd3c154edf0
Guillaume <guillaume@livingdata.co>
2025-09-17 11:10:09 +0200

    Fix colors (dark/light mode)

--- src/components/declarations/month-prevelement-calendar.js
+++ src/components/declarations/month-prevelement-calendar.js
@@ -7,6 +7,7 @@ import {
   format, addDays, startOfWeek, isSameMonth, parse
 } from 'date-fns'
 import {fr} from 'date-fns/locale'
+import {fr as dsfr} from '@codegouvfr/react-dsfr'
 
 const locale = fr
 
@@ -31,7 +32,7 @@ const DayCell = ({day, firstDayCurrentMonth, dataMap, renderTooltipContent, onDa
       cellClasses += ' text-white font-semibold'
     }
   } else if (isCurrentMonthDay) {
-    cellClasses += ' bg-slate-100 hover:bg-slate-200 dark:bg-slate-800 dark:hover:bg-slate-700 text-slate-700 dark:text-slate-300'
+    cellClasses += ' bg-slate-100 hover:bg-slate-200 dark:bg-gray-500 dark:hover:bg-slate-700 text-slate-700 dark:text-slate-300'
   } else {
     cellClasses += ' invisible pointer-events-none'
   }
@@ -101,8 +102,13 @@ const MonthPrelevementCalendar = ({
   const monthNameLocalized = format(firstDayCurrentMonth, 'MMMM yyyy', {locale})
 
   return (
-    <div className='p-3 bg-white dark:bg-slate-900 rounded-lg shadow w-full'>
-      <h2 className='text-lg font-semibold text-center mb-3 text-slate-800 dark:text-slate-200'>
+    <div
+      style={{
+        backgroundColor: dsfr.colors.decisions.background.default
+      }}
+      className='p-3 rounded-lg shadow border w-full'
+    >
+      <h2 className='text-center'>
         {monthNameLocalized.charAt(0).toUpperCase() + monthNameLocalized.slice(1)}
       </h2>
       <div className='grid grid-cols-7 gap-1 mb-2'>
