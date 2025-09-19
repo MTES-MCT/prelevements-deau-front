b75e9ed1591c78efba43a799bdad5fcfa56040dd
Maxine <maxine.lepennec@gmail.com>
2025-09-19 09:13:29 +0200

    Create PeriodTooltip UI component

---
+++ src/components/ui/PeriodTooltip/index.js
@@ -0,0 +1,65 @@
+import {useState, useId} from 'react'
+
+import {Popover, Typography, Box} from '@mui/material'
+import {uniqueId} from 'lodash-es'
+
+import CompactAlert from '@/components/ui/CompactAlert/index.js'
+import MetasList from '@/components/ui/MetasList/index.js'
+
+const PeriodTooltip = ({periodLabel, parameters, alerts, children}) => {
+  const [anchorEl, setAnchorEl] = useState(null)
+  const popoverId = useId()
+
+  const handlePopoverOpen = event => setAnchorEl(event.currentTarget)
+  const handlePopoverClose = () => setAnchorEl(null)
+  const open = Boolean(anchorEl)
+
+  return (
+    <>
+      <span
+        style={{display: 'inline-block'}}
+        onMouseEnter={handlePopoverOpen}
+        onMouseLeave={handlePopoverClose}
+      >
+        {children}
+      </span>
+      <Popover
+        disableRestoreFocus
+        id={popoverId}
+        sx={{
+          pointerEvents: 'none',
+          '& .MuiPaper-root:focus': {outline: 'none'}
+        }}
+        open={open}
+        anchorEl={anchorEl}
+        anchorOrigin={{vertical: 'top', horizontal: 'center'}}
+        transformOrigin={{vertical: 'bottom', horizontal: 'center'}}
+        onClose={handlePopoverClose}
+      >
+        <Box sx={{
+          p: 1, display: 'flex', flexDirection: 'column', gap: 1
+        }}
+        >
+          <Typography sx={{fontWeight: 'bold'}}>{periodLabel}</Typography>
+          <Box sx={{display: 'flex', flexDirection: 'column', gap: 2}}>
+            {parameters?.length > 0 && <MetasList metas={parameters} />}
+
+            {alerts?.length > 0 && (
+              <Box>
+                {alerts.map(alert => (
+                  <CompactAlert
+                    key={uniqueId()}
+                    label={alert.alertLabel}
+                    alertType={alert.alertType}
+                  />
+                ))}
+              </Box>
+            )}
+          </Box>
+        </Box>
+      </Popover>
+    </>
+  )
+}
+
+export default PeriodTooltip
