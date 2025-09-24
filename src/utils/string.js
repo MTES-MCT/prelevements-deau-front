b0e7a593a1925ffdc8643514aeff142c32c8cc2c
Jérôme Desboeufs <jerome@livingdata.co>
2025-09-24 15:06:45 +0200

    Remove dead code

--- src/utils/string.js
+++ src/utils/string.js
@@ -8,16 +8,6 @@ export function normalizeString(string) {
   return deburr(string.toLowerCase())
 }
 
-export function normalizeName(string) {
-  if (!string) {
-    return string
-  }
-
-  return string.split(' ')
-    .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
-    .join(' ')
-}
-
 export function emptyStringToNull(obj) {
   return Object.fromEntries(
     Object.entries(obj).map(([key, value]) =>
