93978cbb0101ca35d2b01bd137ecfdbef820c2cb
GllmR <56537238+GllmR@users.noreply.github.com>
2025-10-15 16:21:45 +0200

    Ajout des modalités (#263)
    * New component ModalitesForm (based on ReglesForm)
    
    * Updates Modalite component
    
    * Display modalités in exploitation page
    
    * Add Modalites form in exploitation form
    
    * Remove unused component
    
    * Fix idx/id

--- src/components/modalite.js
+++ src/components/modalite.js
@@ -1,44 +1,38 @@
 import {Highlight} from '@codegouvfr/react-dsfr/Highlight'
-import {Box, Grid2} from '@mui/material'
+import {Box} from '@mui/material'
 
-const InfoBox = ({label, value}) => (
-  <Box className='flex align-middle justify-between gap-2'>
-    <b>{label} :</b> {value || <i>Non renseigné</i>}
-  </Box>
-)
+const InfoBox = ({label, value}) => {
+  if (!value) {
+    return null
+  }
+
+  return (
+    <Box>
+      <b>{label} :</b> {value}
+    </Box>
+  )
+}
 
 const Modalite = ({modalite}) => (
-  <Grid2
-    container
-    spacing={2}
-    sx={{
-      p: 2,
-      justifyContent: 'space-around'
-    }}
-  >
-    <Box>
-      <InfoBox label='Débit prélevé' value={modalite.freq_debit_preleve} />
-      <InfoBox label='Débit réservé' value={modalite.freq_debit_reserve} />
-      <InfoBox label='Volume prélevé' value={modalite.freq_volume_preleve} />
-    </Box>
-    <Box>
-      <InfoBox label='Niveau Eau' value={modalite.freq_niveau_eau} />
-      <InfoBox label='Chlorure' value={modalite.freq_chlorures} />
-      <InfoBox label='Conductivité' value={modalite.freq_conductivite} />
-    </Box>
-    <Box>
-      <InfoBox label='Nitrates' value={modalite.freq_nitrates} />
-      <InfoBox label='PH' value={modalite.freq_ph} />
-      <InfoBox label='Sulfates' value={modalite.freq_sutlfates} />
-    </Box>
-    <Box>
-      <InfoBox label='Temperature' value={modalite.freq_temperature} />
-      <InfoBox label='Turbidité' value={modalite.freq_turbidite} />
-    </Box>
-    <Highlight>
-      <b>Remarque :</b> <i>{modalite.remarque || 'Non renseigné'}</i>
-    </Highlight>
-  </Grid2>
+  <>
+    <InfoBox label='Débit prélevé' value={modalite.freq_debit_preleve} />
+    <InfoBox label='Débit réservé' value={modalite.freq_debit_reserve} />
+    <InfoBox label='Volume prélevé' value={modalite.freq_volume_preleve} />
+    <InfoBox label='Niveau Eau' value={modalite.freq_niveau_eau} />
+    <InfoBox label='Chlorure' value={modalite.freq_chlorures} />
+    <InfoBox label='Conductivité' value={modalite.freq_conductivite} />
+    <InfoBox label='Nitrates' value={modalite.freq_nitrates} />
+    <InfoBox label='PH' value={modalite.freq_ph} />
+    <InfoBox label='Sulfates' value={modalite.freq_sulfates} />
+    <InfoBox label='Temperature' value={modalite.freq_temperature} />
+    <InfoBox label='Turbidité' value={modalite.freq_turbidite} />
+    {modalite.remarque && (
+      <Highlight>
+        <b>Remarque :</b> <i>{modalite.remarque}</i>
+      </Highlight>
+    )}
+    <hr className='my-2' />
+  </>
 )
 
 export default Modalite
