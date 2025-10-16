e2057bc23677a3efe855aabd9b16489c12535365
Alex Pernot <alex.pernot@livingdata.co>
2025-10-16 10:08:04 +0200

    feat(preleveurs-id-page): adapted the page to the updated mockups

--- src/components/documents/documents-list.js
+++ src/components/documents/documents-list.js
@@ -1,10 +1,19 @@
 import {fr} from '@codegouvfr/react-dsfr'
 
 import Document from '@/components/documents/document.js'
+import SectionCard from '@/components/ui/SectionCard/index.js'
 
-const DocumentsList = ({documents, handleEdit, handleDelete}) => (
-  <div className='border m-3'>
-    {documents.map((d, index) => (
+const DocumentsList = ({idPreleveur, documents, handleEdit, handleDelete}) => (
+  <SectionCard title='Documents' icon='fr-icon-account-line' buttonProps={{
+    children: 'Gérer les documents',
+    iconId: 'fr-icon-draft-line',
+    priority: 'secondary',
+    linkProps: {
+      href: `/preleveurs/${idPreleveur}/documents`
+    }
+  }}
+  >
+    {documents.length > 0 ? documents.map((d, index) => (
       <div
         key={d._id}
         className={`flex w-full even:${fr.colors.decisions.artwork.decorative.pinkTuile.default}`}
@@ -19,8 +28,8 @@ const DocumentsList = ({documents, handleEdit, handleDelete}) => (
           className='w-full'
         />
       </div>
-    ))}
-  </div>
+    )) : (<p><i>Pas de documents</i></p>)}
+  </SectionCard>
 )
 
 export default DocumentsList
