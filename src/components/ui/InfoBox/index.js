ef4cca9aab5dd0e872da6a29c6f6fd63bfc9cf0a
Maxine <maxine.lepennec@gmail.com>
2025-10-16 09:01:51 +0200

    Improve modalites

---
+++ src/components/ui/InfoBox/index.js
@@ -0,0 +1,33 @@
+import {fr} from '@codegouvfr/react-dsfr'
+import {Box, Typography} from '@mui/material'
+
+const InfoBox = ({icon, label, description}) => {
+  if (!label || !description) {
+    return null
+  }
+
+  return (
+    <Box
+      sx={{
+        display: 'inline-flex',
+        alignItems: 'center',
+        padding: '5px',
+        gap: '5px',
+        border: `1px solid ${fr.colors.decisions.border.default.grey.default}`,
+        backgroundColor: fr.colors.decisions.background.default.grey.default
+      }}
+    >
+
+      <span style={{color: fr.colors.decisions.text.label.blueFrance.default}}>
+        {icon}
+      </span>
+
+      <Box className='flex items-center gap-1'>
+        <Typography fontWeight='bold' variant='body1'>{label} :</Typography>
+        <Typography>{description}</Typography>
+      </Box>
+    </Box>
+  )
+}
+
+export default InfoBox
---
+++ src/components/ui/InfoBox/info-box.stories.js
@@ -0,0 +1,32 @@
+import {Star} from '@mui/icons-material'
+
+import InfoBox from './index.js'
+
+const meta = {
+  title: 'Components/InfoBox',
+  component: InfoBox,
+  tags: ['autodocs'],
+  argTypes: {
+    icon: {
+      control: 'object',
+      description: 'Icône affichée avant le label. Type: Object. Obligatoire.'
+    },
+    label: {
+      control: 'text',
+      description: 'Texte du label affiché. Type: string. Obligatoire.'
+    },
+    description: {
+      control: 'text',
+      description: 'Texte de la description affichée. Type: string. Obligatoire.'
+    }
+  },
+  args: {
+    icon: <Star />,
+    label: 'Label',
+    description: 'valeur associée au label'
+  }
+}
+
+export default meta
+
+export const Défaut = {}
