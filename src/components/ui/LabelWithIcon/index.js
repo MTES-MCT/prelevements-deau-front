9a941b7bd1496099d3effe403d27e297beb3e8bf
Maxine <maxine.lepennec@gmail.com>
2025-10-27 15:22:22 +0100

    Handle LabelWithIcon if icon is missing

--- src/components/ui/LabelWithIcon/index.js
+++ src/components/ui/LabelWithIcon/index.js
@@ -4,9 +4,11 @@ import Icon from '@/components/ui/Icon/index.js'
 
 const LabelWithIcon = ({icon, iconId, children}) => (
   <Box className='flex flex-wrap items-center gap-2'>
-    <Typography variant='body1' color='primary'>
-      <Icon iconElement={icon} iconId={iconId} />
-    </Typography>
+    {(icon || iconId) && (
+      <Typography variant='body2' color='primary'>
+        <Icon iconElement={icon} iconId={iconId} />
+      </Typography>
+    )}
     {children || (
       <Typography variant='body2'>
         <i>Non renseigné</i>
